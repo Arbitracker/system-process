727b1f7dfb78dcaab37fd1d9211d5bb83fab81c9
Jakob Westhoff <jakob@westhoffswelt.de>
2008-11-13 18:55:48 +0000

    - Moved systemProcess to its own repository
    git-svn-id: svn://pureenergy.cc/systemProcess/trunk@1 31603df9-db43-4f3c-b741-60c6c1c776c2

---
+++ src/autoload/systemProcess.php
@@ -0,0 +1,8 @@
+<?php
+
+return array( 
+    'pbsSystemProcess'                                      =>  'classes/systemProcess.php',
+    'pbsSystemProcessNotRunningException'                   =>  'exceptions/systemProcess/notRunning.php',
+    'pbsSystemProcessInvalidCustomDescriptorException'      =>  'exceptions/systemProcess/invalidCustomDescriptor.php',
+    'pbsSystemProcessRecursivePipeException'                =>  'exceptions/systemProcess/recursivePipe.php',
+);
